52e64cedd15c54666e7ab3127dc606bcdda4371a
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2023-02-24 08:14:27 -0300

    add atv10

--- lista1702/atv10.c
+++ lista1702/atv10.c
@@ -7,20 +7,25 @@
 int main() {
     int array[t];
     int *ptr = array;
-    int i;
-    for ( i= 0; i < t ; i++) {
-        printf("digite um valor pra variavel %i:",i);
-        scanf("%d", (ptr+i));
-    
-    }
-    dobrar(ptr);
+
+    *ptr=SolicitarValor(ptr);
+     dobrar(ptr);
 
 }
-void dobrar(int *p){
+int SolicitarValor(int *p)
+{ 
     int i;
-        for ( i= 0; i < t ; i++) {
-        printf("\no dobro da variavel %i e igual: %d",i , *(p+i)*2);
-    
+    for ( i= 0; i < t ; i++) 
+    {
+        printf("digite um valor pra variavel %i:",i);
+        scanf("%d", (p+i));
     }
-
+}
+void dobrar(int *p)
+{
+    int i;
+        for ( i= 0; i < t ; i++) 
+        {
+            printf("\no dobro da variavel %i e igual: %d",i , *(p+i)*2);
+        }
 }
